5fcc685e0ec42edbe422d94f73a3e5d87f48778e
Karthik G <kartg@paypal.com>
2018-09-16 23:47:19 +0530

    CRUD operation on sqlite DB

--- server.js
+++ server.js
@@ -12,14 +12,16 @@ var deletesqlSchema = fs.readFileSync('app/data/user-drop-schema.sql').toString(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if(!fs.existsSync('app/data/api.db')){
+if (!fs.existsSync('app/data/api.db')) {
     fs.openSync('app/data/api.db');
 }
 db = new sqlite3.Database('app/data/api.db');
 
-db.serialize(function() {
-    db.run(deletesqlSchema);
-    db.run(createsqlSchema);
+db.serialize(function () {
+    db.run(deletesqlSchema, function () {
+        db.run(createsqlSchema);
+    });
+
 });
 
 app.get('/api', (req, res) => {
@@ -28,20 +30,39 @@ app.get('/api', (req, res) => {
 
 app.post('/api', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    var data = req.body;
-    processUsers(req, res, db);
+    insertUser(req.body, res, db, (err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.status(200).send("CREATE ENTRY SUCCESSFUL");
+    });
 });
 
 app.put('/api', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-     var data = req.body;
-     processPut(req, res, db);
+    processPut(req, res, db);
 });
 
 app.delete('/api', (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    var data = req.body;
-    deleteUser(req, res, db);
+    deleteUsers(db, (err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.status(200).send("DELETE COLLECTION SUCCESSFUL");
+    });
+});
+
+app.put('/api/:id', (req, res) => {
+    processPut(req, res, db);
+});
+
+app.delete('/api/:id', (req, res) => {
+    deleteUser(req.params.id, res, db);
+});
+
+app.get('/api/:id', (req, res) => {
+    processData(res, "SELECT * FROM users where userid= ?", req.params.id);
 });
 
 
@@ -49,107 +70,118 @@ app.listen(port, () => {
     console.log('Backend NodeJS live on ' + port);
 });
 
-function processPut(req,res,db){
-    for (var user of req.body) {
-        updateUser(user, res, db);
-    } 
+function deleteUsers(db, callback) {
+    var sql = `delete from  Users;`;
+    db.serialize(function () {
+        db.run(sql, function (err) {
+            if (err) {
+                console.error(err);
+                callback(err);
+            }
+            callback();
+        });
+    });
 }
 
-function processUsers(req, res, db) {
-    for (var user of req.body) {
-        insertUser(user, res, db);
-    }
-}
-
-function insertUser(product, res, db) {
-    var name = product.name;
-    var description = product.description;
-    var price = product.price;
-    var currency = product.currency;
-
+function insertUser(user, res, db, callback) {
+    var name = user.name;
+    var email = user.email;
+    var phone = user.phone;
     var sql = `insert into Users (name, email, phone) 
             VALUES 
             (?, ?, ?);`;
-
-    var values = [name, description, price, currency];
-
+    var values = [name, email, phone];
     db.serialize(function () {
         db.run(sql, values, function (err) {
             if (err) {
                 console.error(err);
-                res.status(500).send(err);
+                callback(err);
             }
-
-            else
-                res.send();
+            callback();
         });
     });
 }
 
-function updateUser(user, res, db){
+function processPut(req, res, db) {
+    if (Array.isArray(req.body)) {
+        req.body.forEach((user, i) => {
+            updateUser(user, db, ++i, (err) => {
+                if (err)
+                    res.status(500).send(err);
+                else {
+                    if (i === req.body.length) {
+                        res.status(200).send("REPLACE COLLECTION SUCCESSFUL");
+                    }
+                }
+
+            });
+        });
+    } else {
+        updateUser(req.body, db, req.params.id, (err) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            res.status(200).send("UPDATE ITEM SUCCESSFUL");
+        });
+    }
+
+}
+
+function updateUser(user, db, id, callback) {
     var name = user.name;
     var email = user.email;
     var phone = user.phone;
-    var id = user.id;
-
-    var sql = `update Products
+    var sql = `update Users
             set name = ?, email = ?, phone = ?
-            where id = ?;`;
-
+            where userid = ?;`;
     var values = [name, email, phone, id];
-
     db.serialize(function () {
         db.run(sql, values, function (err) {
-            if (err){
+            if (err) {
                 console.error(err);
-                res.status(500).send(err);
+                callback(err);
             }
-            else
-                res.send();
+            callback();
         });
     });
 }
 
-function deleteUser(user, res, db){
-    var id = user.id;
-
-    if(!id){
+function deleteUser(id, res, db) {
+    if (!id) {
         res.status(400).send("ID is mandatory");
     }
-
-    else{
-        var sql = `delete from  Users where id = ?;`;
+    else {
+        var sql = `delete from  Users where userid = ?;`;
         var values = [id];
-
         db.serialize(function () {
             db.run(sql, values, function (err) {
-                if (err){
-                    console.error(err);
-                    res.status(500).send(err);
-                }
-                else
-                    res.send();
-            });
-        });
-    }
-}
-
-
-function processData(res, sql) {
-    db.serialize(function () {
-        db.all(sql,
-            function (err, rows) {
                 if (err) {
                     console.error(err);
                     res.status(500).send(err);
                 }
                 else
-                    sendData(res, rows, err);
+                    res.status(200).send("DELETE ITEM SUCCESSFUL");
+            });
+        });
+    }
+}
+
+
+function processData(res, sql, params) {
+    params = params ? [params] : []
+    db.serialize(function () {
+        db.all(sql, params,
+            function (err, rows) {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send(err);
+                }
+                sendData(res, rows);
             });
     });
 }
 
-function sendData(res, data, err) {
+function sendData(res, data) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     if (data[0])
         res.send(data);
